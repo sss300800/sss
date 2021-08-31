130519bf4c9c86e4cb08b91c89047dd50ee06408
Somnath Sopan Shirsath <83588073+sss300800@users.noreply.github.com>
2021-08-31 17:08:03 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace factorialprogram
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int i, fact = 1, number;
+            Console.Write("enter number:");
+            number = int.Parse(Console.ReadLine());
+            Console.Read();
+            for(i=1;i<=number;i++)
+            {
+                fact=fact*i;
+            }
+            Console.Write("factorial of:" + number + "is"+fact);
+            
+            
+
+
+
+        }
+    }
+}
